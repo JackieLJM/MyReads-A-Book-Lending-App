737bc4d864c4c69b50456ccbc97ec79e0079b051
JackieLJM <ljmlionshy@live.com>
2017-07-27 16:04:59 +0800

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import {Router} from 'react-router-dom';
-import * as BooksAPI from './BooksAPI';
+import ListBooks from './ListBooks.js';
+import SearchBooks from './SearchBooks.js'
+// import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+  state = {}
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          
-        ) : (
-          
-        )}
+        <Router path='/' render={< ListBooks />}/>
+        <Router path='/search' render={< SearchBooks />}/>
       </div>
     )
   }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-
-<div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
+const SearchBooks = (props) => (
+  <div className="search-books">
+    <div className="search-books-bar">
+      <Link className="close-search" to='/'>Close</Link>
+      <div className="search-books-input-wrapper">
+        {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -12,12 +14,14 @@
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+        <input type="text" placeholder="Search by title or author"/>
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-          export SearchBooks;
+      </div>
+    </div>
+    <div className="search-books-results">
+      <ol className="books-grid"></ol>
+    </div>
+  </div>
+);
+
+export default SearchBooks;
